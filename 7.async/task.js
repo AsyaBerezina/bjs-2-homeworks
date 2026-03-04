faf0354dc724020cc93d06b1a56bfcec73046478
Anastasiya Berezina <bask1371@yandex.ru>
2026-03-04 23:58:21 +0300

    задание по теме асинхронность готово

---
+++ 7.async/task.js
@@ -0,0 +1,73 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error('Отсутствуют обязательные аргументы');
+    }
+
+    const hasSameTime = this.alarmCollection.some(function (alarm) {
+      return alarm.time === time;
+    });
+
+    if (hasSameTime) {
+      console.warn('Уже присутствует звонок на это же время');
+    }
+
+    this.alarmCollection.push({
+      time: time,
+      callback: callback,
+      canCall: true,
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(function (alarm) {
+      return alarm.time !== time;
+    });
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+
+    return hours + ':' + minutes;
+  }
+
+  start() {
+    if (this.intervalId) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(function (alarm) {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(function (alarm) {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
